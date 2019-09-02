cc941738900845a9d1479ef7d16c24732fd6f921
amaretto <jabberwock26@gmail.com>
2019-09-03 01:24:41 +0900

    add waveform test code

--- cmd/test/waveform/main.go
+++ cmd/test/waveform/main.go
@@ -1,18 +1,14 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
+	"log"
+	"math"
 	"os"
-	"time"
 
-	"github.com/faiface/beep"
-	gomp3 "github.com/hajimehoshi/go-mp3"
-)
-
-const (
-	gomp3NumChannels   = 2
-	gomp3Precision     = 2
-	gomp3BytesPerFrame = gomp3NumChannels * gomp3Precision
+	"github.com/faiface/beep/mp3"
+	//gomp3 "github.com/hajimehoshi/go-mp3"
 )
 
 func main() {
@@ -21,22 +17,49 @@ func main() {
 	if err != nil {
 		report(err)
 	}
-	d, err := gomp3.NewDecoder(f)
+	streamer, _, err := mp3.Decode(f)
+
+	cf, err := os.OpenFile("hoge.csv", os.O_WRONLY|os.O_CREATE, 0600)
 	if err != nil {
-		report(err)
+		log.Fatal("Error:", err)
+	}
+	defer cf.Close()
+	err = cf.Truncate(0)
+	if err != nil {
+		log.Fatal("Error:", err)
 	}
 
-	format := beep.Format{
-		SampleRate:  beep.SampleRate(d.SampleRate()),
-		NumChannels: gomp3NumChannels,
-		Precision:   gomp3Precision,
+	writer := csv.NewWriter(cf)
+
+	var tmp [2][2]float64
+	var count int
+	for {
+		// EOF
+		if sn, sok := streamer.Stream(tmp[:1]); sn == 0 && !sok {
+			break
+		}
+		samplel := tmp[0][0]
+		sampler := tmp[0][1]
+
+		sumSquare := math.Pow(samplel, 2)
+		sumSquare += math.Pow(sampler, 2)
+		value := math.Sqrt(sumSquare)
+
+		posstr := fmt.Sprint(streamer.Position())
+		valstr := fmt.Sprint(value)
+
+		writer.Write([]string{posstr, valstr})
+
+		count++
+		//fmt.Printf("pos:%v,l:%v, r:%v\n", streamer.Position(), samplel, sampler)
+
+		//fmt.Printf("pos:%v,value:%v\n", streamer.Position(), value)
+		if count == 1000000 {
+			break
+		}
 	}
-
-	bufferSize := format.SampleRate.N(time.Second / 30)
-	fmt.Println(bufferSize)
-
-	//samples := make([][2]float64, bufferSize)
-
+	writer.Flush()
+	//fmt.Println(count)
 }
 
 func report(err error) {
