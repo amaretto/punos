9a596edbb955bc4f50516f61dc3c1f5c85e87faa
amaretto <jabberwock26@gmail.com>
2020-07-19 23:56:39 +0900

    add gitignore

--- pkg/cmd/cli/turntable/app.go
+++ pkg/cmd/cli/turntable/app.go
@@ -1,7 +1,16 @@
 package turntable
 
+import "github.com/rivo/tview"
+
 // App is standalone turntable(music player) application
 type App struct {
-	ttp *PlayPanel
-	sp  *SelectPanel
+	app *tview.Application
+
+	pp *PlayPanel
+	sp *SelectPanel
+}
+
+// Stop stop the application
+func (a *App) Stop() {
+	a.app.Stop()
 }
--- pkg/cmd/cli/turntable/turntable_panel.go
+++
@@ -1,6 +0,0 @@
-package turntable
-
-// PlayPanel give some functions of music player
-type PlayPanel struct {
-	Panel
-}
