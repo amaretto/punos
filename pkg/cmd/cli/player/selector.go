4859531d58111a294a83c4b6c1e2c2d62a012212
amaretto <jabberwock26@gmail.com>
2020-12-20 02:21:45 +0900

    Start to implement init functions

--- pkg/cmd/cli/player/selector.go
+++ pkg/cmd/cli/player/selector.go
@@ -1,6 +1,9 @@
 package player
 
 import (
+	"fmt"
+
+	"github.com/gdamore/tcell"
 	"github.com/rivo/tview"
 )
 
@@ -28,24 +31,25 @@ func newSelector(app *App) *Selector {
 	s.SetDirection(tview.FlexRow).
 		AddItem(tview.NewFlex().SetDirection(tview.FlexColumn).
 			AddItem(s.turntableID, 0, 2, false).
-			AddItem(s.djID, 0, 2, false).
+			AddItem(s.djID, 0, 2, true).
 			AddItem(s.musicTitle, 0, 3, false), 0, 1, false)
 
+	s.SetKeyHandler()
 	return s
 }
 
-//func (s *Selector) setKeyHandler() {
-//	s.SetInputCapture(func(e *tcell.EventKey) *tcell.EventKey {
-//		s.app.SetGlobalKeyBinding(e)
-//		switch e.Key() {
-//		case tcell.KeyESC:
-//			s.app.Stop()
-//		}
-//
-//		switch e.Rune() {
-//		case 'b':
-//			fmt.Println("fuag")
-//		}
-//		return e
-//	})
-//}
+func (s *Selector) SetKeyHandler() {
+	s.SetInputCapture(func(e *tcell.EventKey) *tcell.EventKey {
+		s.app.SetGlobalKeyBinding(e)
+		switch e.Key() {
+		case tcell.KeyESC:
+			s.app.Stop()
+		}
+
+		switch e.Rune() {
+		case 'b':
+			fmt.Println("fuag")
+		}
+		return e
+	})
+}
