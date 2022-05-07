9bf3232dbb21efcd261dcf4afb516b7481115b73
amaretto <jabberwock26@gmail.com>
2022-05-07 16:40:58 +0900

    set default subcommand

--- cmd/punos/main.go
+++ cmd/punos/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"os"
+
 	"github.com/amaretto/punos/pkg/cmd/punos"
 )
 
 func main() {
+	// default command is player
+	if len(os.Args) == 1 {
+		os.Args = append([]string{os.Args[0], "player"}, os.Args[1:]...)
+	}
 	punos.NewCommand().Execute()
 }
